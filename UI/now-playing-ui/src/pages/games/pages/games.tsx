cee119dd87f6cd05f6f34b452dd3c06b6c78f801
Mike Gonzalez <is722298@iteso.mx>
2025-04-10 00:08:23 -0600

    added update info btn

--- UI/now-playing-ui/src/pages/games/pages/games.tsx
+++ UI/now-playing-ui/src/pages/games/pages/games.tsx
@@ -1,32 +1,52 @@
-import { Box, Grid, Skeleton, Typography } from '@mui/material';
-import SideBar from '../../../components/sideBar';
-import { SteamGame, PsnGame } from '../utils/types'; // Import interfaces
-import GameCard from '../components/gameCard';
-import { parseDate, getPlaytime, calculateAchievementPercentage } from '../utils/utils';
+import {
+    Box,
+    Grid,
+    Skeleton,
+    Typography,
+    Snackbar,
+    Button,
+    IconButton,
+    Alert,
+} from "@mui/material";
+import RefreshIcon from "@mui/icons-material/Refresh";
+import SideBar from "../../../components/sideBar";
+import { SteamGame, PsnGame } from "../utils/types"; // Import interfaces
+import GameCard from "../components/gameCard";
+import {
+    parseDate,
+    getPlaytime,
+    calculateAchievementPercentage,
+} from "../utils/utils";
 
-import { Link } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function Games() {
-    const drawerWidth = 160
+    const drawerWidth = 160;
 
     const [steamGames, setSteamGames] = useState<SteamGame[]>([]);
     const [psnGames, setPsnGames] = useState<PsnGame[]>([]);
-    const [latestPlayedGames, setMergedGames] = useState<(SteamGame | PsnGame)[]>([]);
+    const [latestPlayedGames, setMergedGames] = useState<(SteamGame | PsnGame)[]>(
+        []
+    );
     const [mostPlayed, setMostPlayed] = useState<(SteamGame | PsnGame)[]>([]);
     const [mostAchieved, setMostAchieved] = useState<(SteamGame | PsnGame)[]>([]);
 
     const [loading, setLoading] = useState(true);
+    const [snackbarOpen, setSnackbarOpen] = useState(false); // State to control the snackbar visibility
+    const [error, setError] = useState<string | null>(null); // Error state
 
-
-    function mergeAndSortGames(steamArray: SteamGame[], psnArray: PsnGame[]): (SteamGame | PsnGame)[] {
+    function mergeAndSortGames(
+        steamArray: SteamGame[],
+        psnArray: PsnGame[]
+    ): (SteamGame | PsnGame)[] {
         const invalidTimestamp = new Date(1970, 0, 1).getTime();
         const mergedGames = [...steamArray, ...psnArray]
-            .map(game => ({
+            .map((game) => ({
                 ...game,
                 lastPlayed: parseDate(game.last_played),
             }))
-            .filter(game => game.lastPlayed.getTime() !== invalidTimestamp)
+            .filter((game) => game.lastPlayed.getTime() !== invalidTimestamp)
             .sort((a, b) => b.lastPlayed.getTime() - a.lastPlayed.getTime());
         return mergedGames;
     }
@@ -37,6 +57,37 @@ function Games() {
         return Object.values(data.result);
     };
 
+    const fetchLatestPlayedGames = async () => {
+        try {
+            setSnackbarOpen(true);
+            const res = await fetch("http://localhost:8000/steam/get-game-list/");
+
+            if (!res.ok) {
+                setError("Failed to fetch recently played steam games.");
+                setLoading(false);
+                setSnackbarOpen(false); // Hide snackbar when fetch fails
+                return;
+            }
+            const resPSN = await fetch("http://localhost:8000/psn/get-game-list/");
+
+            if (!resPSN.ok) {
+                setError("Failed to fetch recently played PSN games.");
+                setLoading(false);
+                setSnackbarOpen(false); // Hide snackbar when fetch fails
+                return;
+            }
+            await fetchGames();
+
+            setError(null);
+            setLoading(false);
+            setSnackbarOpen(false);
+        } catch (error) {
+            console.error("Error fetching recently played games:", error);
+            setError("An error occurred while fetching recently played games");
+            setLoading(false);
+            setSnackbarOpen(false); // Hide snackbar when an error occurs
+        }
+    };
     const fetchGames = async () => {
         try {
             setLoading(true);
@@ -48,18 +99,18 @@ function Games() {
                 steamMostAchievedArray,
                 psnMostAchievedArray,
             ] = await Promise.all([
-                fetchGameData('http://localhost:8000/steam/get-game-list-stored/'),
-                fetchGameData('http://localhost:8000/psn/get-game-list-stored/'),
-                fetchGameData('http://localhost:8000/steam/get-game-list-total-playtime/'),
-                fetchGameData('http://localhost:8000/psn/get-game-list-total-playtime/'),
-                fetchGameData('http://localhost:8000/steam/get-game-list-most-achieved/'),
-                fetchGameData('http://localhost:8000/psn/get-game-list-most-achieved/'),
+                fetchGameData("http://localhost:8000/steam/get-game-list-stored/"),
+                fetchGameData("http://localhost:8000/psn/get-game-list-stored/"),
+                fetchGameData("http://localhost:8000/steam/get-game-list-total-playtime/"),
+                fetchGameData("http://localhost:8000/psn/get-game-list-total-playtime/"),
+                fetchGameData("http://localhost:8000/steam/get-game-list-most-achieved/"),
+                fetchGameData("http://localhost:8000/psn/get-game-list-most-achieved/"),
             ]);
 
             setSteamGames(steamArray);
             setPsnGames(psnArray);
             const merged = mergeAndSortGames(steamArray, psnArray);
-            
+
             setMergedGames(merged);
 
             const mergedPlaytimeGames = [...steamPlaytimeArray, ...psnPlaytimeArray]
@@ -71,7 +122,10 @@ function Games() {
                 });
             setMostPlayed(mergedPlaytimeGames);
 
-            const mergedMostAchievedGames = [...steamMostAchievedArray, ...psnMostAchievedArray]
+            const mergedMostAchievedGames = [
+                ...steamMostAchievedArray,
+                ...psnMostAchievedArray,
+            ]
                 .map((game) => {
                     return {
                         ...game,
@@ -101,24 +155,50 @@ function Games() {
             </Grid>
         ));
 
+    const handleSnackbarClose = () => {
+        setSnackbarOpen(false);
+    };
     return (
         <div>
-            <Box sx={{ display: 'flex', paddingLeft: 2.5 }}>
+            <Box sx={{ display: "flex", paddingLeft: 2.5 }}>
                 <SideBar activeItem="Games" />
-                <Box
-                    component="main"
-                    sx={{ width: '89vw', }}
-                >
-                    <Typography variant="h5" sx={{
-                        mt: 2, ml: 1, fontFamily: 'Inter, sans-serif',
-                        fontWeight: 700
-                    }}>
-                        Now Playing 🎮
-                    </Typography>
+                <Box component="main" sx={{ width: "89vw" }}>
                     <Box
                         sx={{
-                            display: 'flex',
-                            overflowX: 'auto',
+                            display: "flex",
+                            justifyContent: "space-between",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Typography
+                            variant="h5"
+                            sx={{
+                                mt: 2,
+                                ml: 1,
+                                fontFamily: "Inter, sans-serif",
+                                fontWeight: 700,
+                            }}
+                        >
+                            Now Playing 🎮
+                        </Typography>
+                        <IconButton
+                            onClick={fetchLatestPlayedGames}
+                            color="secondary"
+                            sx={{ mb: -2 }}
+                        >
+                            <RefreshIcon />
+                        </IconButton>
+                    </Box>
+                    {/* Display error banner if there is an error */}
+                    {error && (
+                        <Alert severity="error" sx={{ mb: 2 }}>
+                            {error}
+                        </Alert>
+                    )}
+                    <Box
+                        sx={{
+                            display: "flex",
+                            overflowX: "auto",
                             gap: 32,
                             py: 1,
                             px: 1,
@@ -131,23 +211,28 @@ function Games() {
                                     <Link
                                         to={`/game/${game.appid}`}
                                         state={{ game }} // Pass the game object here
-                                        style={{ textDecoration: 'none' }}
+                                        style={{ textDecoration: "none" }}
                                     >
                                         <GameCard game={game} />
                                     </Link>
                                 </Grid>
                             ))}
                     </Box>
-                    <Typography variant="h5" sx={{
-                        mt: 2, ml: 1, fontFamily: 'Inter, sans-serif',
-                        fontWeight: 700
-                    }}>
+                    <Typography
+                        variant="h5"
+                        sx={{
+                            mt: 2,
+                            ml: 1,
+                            fontFamily: "Inter, sans-serif",
+                            fontWeight: 700,
+                        }}
+                    >
                         Most Played ⌛
                     </Typography>
                     <Box
                         sx={{
-                            display: 'flex',
-                            overflowX: 'auto',
+                            display: "flex",
+                            overflowX: "auto",
                             gap: 32,
                             py: 1,
                             px: 1,
@@ -160,23 +245,28 @@ function Games() {
                                     <Link
                                         to={`/game/${game.appid}`}
                                         state={{ game }} // Pass the game object here
-                                        style={{ textDecoration: 'none' }}
+                                        style={{ textDecoration: "none" }}
                                     >
                                         <GameCard game={game} />
                                     </Link>
                                 </Grid>
                             ))}
                     </Box>
-                    <Typography variant="h5" sx={{
-                        mt: 2, ml: 1, fontFamily: 'Inter, sans-serif',
-                        fontWeight: 700
-                    }}>
+                    <Typography
+                        variant="h5"
+                        sx={{
+                            mt: 2,
+                            ml: 1,
+                            fontFamily: "Inter, sans-serif",
+                            fontWeight: 700,
+                        }}
+                    >
                         Most Achieved 🏆
                     </Typography>
                     <Box
                         sx={{
-                            display: 'flex',
-                            overflowX: 'auto',
+                            display: "flex",
+                            overflowX: "auto",
                             gap: 32,
                             py: 1,
                             px: 1,
@@ -189,7 +279,7 @@ function Games() {
                                     <Link
                                         to={`/game/${game.appid}`}
                                         state={{ game }} // Pass the game object here
-                                        style={{ textDecoration: 'none' }}
+                                        style={{ textDecoration: "none" }}
                                     >
                                         <GameCard game={game} />
                                     </Link>
@@ -198,7 +288,18 @@ function Games() {
                     </Box>
                 </Box>
             </Box>
-        </div >
+            <Snackbar
+                open={snackbarOpen}
+                autoHideDuration={null} // Snackbar will stay open until closed manually
+                onClose={handleSnackbarClose}
+                message="Updating info..."
+                action={
+                    <Button color="secondary" size="small" onClick={handleSnackbarClose}>
+                        Close
+                    </Button>
+                }
+            />
+        </div>
     );
 }
 
--- UI/now-playing-ui/src/pages/movies/pages/movies.tsx
+++ UI/now-playing-ui/src/pages/movies/pages/movies.tsx
@@ -1,26 +1,67 @@
-
-import { Box, Typography } from '@mui/material';
-import SideBar from '../../../components/sideBar';
-import { useState, useEffect } from 'react';
-import { Movie, Show } from '../utils/types';
-import MediaCard from '../components/mediaCard';
+import {
+    Alert,
+    Box,
+    Button,
+    IconButton,
+    Snackbar,
+    Typography,
+} from "@mui/material";
+import SideBar from "../../../components/sideBar";
+import { useState, useEffect } from "react";
+import { Movie, Show } from "../utils/types";
+import MediaCard from "../components/mediaCard";
+import RefreshIcon from "@mui/icons-material/Refresh";
 
 const fetchMoviesData = async (): Promise<Movie[]> => {
-    const res = await fetch('http://localhost:8000/trakt/get-stored-movies');
+    const res = await fetch("http://localhost:8000/trakt/get-stored-movies");
     const data = await res.json();
     return data.movies;
 };
 
 const fetchShowsData = async (): Promise<Show[]> => {
-    const res = await fetch('http://localhost:8000/trakt/get-stored-shows');
+    const res = await fetch("http://localhost:8000/trakt/get-stored-shows");
     const data = await res.json();
     return data.shows;
 };
 
-
 function Movies() {
     const [movies, setMovies] = useState<Movie[]>([]);
     const [shows, setShows] = useState<Show[]>([]);
+    const [snackbarOpen, setSnackbarOpen] = useState(false); // State to control the snackbar visibility
+    const [error, setError] = useState<string | null>(null); // Error state
+    const [loading, setLoading] = useState(true);
+
+    const fetchLatest = async () => {
+        try {
+            setSnackbarOpen(true);
+            const res = await fetch("http://localhost:8000/trakt/fetch-latest-movies/");
+            if (!res.ok) {
+                setError("Failed to fetch recently played movies.");
+                setLoading(false);
+                setSnackbarOpen(false); // Hide snackbar when fetch fails
+                return;
+            }
+            const resShows = await fetch(
+                "http://localhost:8000/trakt/fetch-latest-shows/"
+            );
+            if (!resShows.ok) {
+                setError("Failed to fetch recently palyes tv shows.");
+                setLoading(false);
+                setSnackbarOpen(false); // Hide snackbar when fetch fails
+                return;
+            }
+            await fetchMoviesData();
+            await fetchShowsData();
+            setError(null);
+            setLoading(false);
+            setSnackbarOpen(false); // Hide snackbar when fetch is successful
+        } catch (error) {
+            console.error("Error fetching recently played tracks:", error);
+            setError("An error occurred while fetching recently played tracks.");
+            setLoading(false);
+            setSnackbarOpen(false); // Hide snackbar when an error occurs
+        }
+    };
     useEffect(() => {
         const fetchData = async () => {
             const moviesData = await fetchMoviesData();
@@ -31,25 +72,47 @@ function Movies() {
 
         fetchData();
     }, []);
+    const handleSnackbarClose = () => {
+        setSnackbarOpen(false);
+    };
     return (
         <div>
-            <Box sx={{ display: 'flex' }}>
+            <Box sx={{ display: "flex" }}>
                 <SideBar activeItem="Movies" /> {/* Highlight "Movies" */}
-                <Box
-                    component="main"
-                    sx={{ width: '89vw', padding: 2, }}
-                >
-                    <Typography variant="h5" sx={{
-                        mt: 2, ml: 1, fontFamily: 'Inter, sans-serif',
-                        fontWeight: 700
-                    }}>
-                        Now Playing 📽️
-                    </Typography>
+                <Box component="main" sx={{ width: "89vw", padding: 2 }}>
                     <Box
                         sx={{
-                            display: 'flex',
-                            width: '100%',
-                            overflowX: 'scroll',
+                            display: "flex",
+                            justifyContent: "space-between",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Typography
+                            variant="h5"
+                            sx={{
+                                mt: 2,
+                                ml: 1,
+                                fontFamily: "Inter, sans-serif",
+                                fontWeight: 700,
+                            }}
+                        >
+                            Now Playing 📽️
+                        </Typography>
+                        <IconButton onClick={fetchLatest} color="secondary" sx={{ mb: -2 }}>
+                            <RefreshIcon />
+                        </IconButton>
+                    </Box>
+                    {/* Display error banner if there is an error */}
+                    {error && (
+                        <Alert severity="error" sx={{ mb: 2 }}>
+                            {error}
+                        </Alert>
+                    )}
+                    <Box
+                        sx={{
+                            display: "flex",
+                            width: "100%",
+                            overflowX: "scroll",
                             gap: 2,
                             py: 1,
                             px: 1,
@@ -62,39 +125,56 @@ function Movies() {
                         ) : (
                             <Typography variant="body1" sx={{ ml: 1 }}>
                                 No movies available.
-                            </Typography>)
-                        }
-
+                            </Typography>
+                        )}
                     </Box>
-                    <Typography variant="h5" sx={{
-                        mt: 2, ml: 1, fontFamily: 'Inter, sans-serif',
-                        fontWeight: 700
-                    }}>
+                    <Typography
+                        variant="h5"
+                        sx={{
+                            mt: 2,
+                            ml: 1,
+                            fontFamily: "Inter, sans-serif",
+                            fontWeight: 700,
+                        }}
+                    >
                         Now Playing 📺
                     </Typography>
                     <Box
                         sx={{
-                            display: 'flex',
-                            width: '100%',
-                            overflowX: 'scroll',
+                            display: "flex",
+                            width: "100%",
+                            overflowX: "scroll",
                             gap: 2,
                             py: 1,
                             px: 1,
                         }}
                     >
-                        {shows && shows.length > 0 ? (
-                            shows.map((show: Show) => (
-                                <MediaCard media={show} mediaType="show" key={show.show.ids.tmdb}/>
-                            ))
-                        ) : (
-                            <Typography variant="body1" sx={{ ml: 1 }}>
-                                No shows available.
-                            </Typography>) // Use MediaCard component
+                        {
+                            shows && shows.length > 0 ? (
+                                shows.map((show: Show) => (
+                                    <MediaCard media={show} mediaType="show" key={show.show.ids.tmdb} />
+                                ))
+                            ) : (
+                                <Typography variant="body1" sx={{ ml: 1 }}>
+                                    No shows available.
+                                </Typography>
+                            ) // Use MediaCard component
                         }
                     </Box>
                 </Box>
             </Box>
-        </div >
+            <Snackbar
+                open={snackbarOpen}
+                autoHideDuration={null} // Snackbar will stay open until closed manually
+                onClose={handleSnackbarClose}
+                message="Updating info..."
+                action={
+                    <Button color="secondary" size="small" onClick={handleSnackbarClose}>
+                        Close
+                    </Button>
+                }
+            />
+        </div>
     );
 }
 
