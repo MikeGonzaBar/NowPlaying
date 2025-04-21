a0a54836194fbbc229fb64c8a9155e77473defe1
Mike Gonzalez <is722298@iteso.mx>
2025-04-21 10:44:11 -0600

    Updated CORS settings to allow all origins for improved API accessibility.

--- API/NowPlayingAPI/NowPlayingAPI/settings.py
+++ API/NowPlayingAPI/NowPlayingAPI/settings.py
@@ -151,9 +151,4 @@ REST_FRAMEWORK = {
     "PAGE_SIZE": 10,
 }
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",  # Frontend origin
-    "http://127.0.0.1:5173",
-    "http://localhost:5173",
-    "http://localhost:3000"
-]
+CORS_ALLOW_ALL_ORIGINS = True
